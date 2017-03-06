2bdfd527a8009556c701c5f130af3d943e5b37ef
HermonMulat <hermonmulat@gmail.com>
2017-03-06 20:57:45 +0100

    https://open.kattis.com/problems/falling

---
+++ mugs.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+def main():
+
+    D = int(sys.stdin.readline().strip()) # distance
+    sqrt_D = int(math.sqrt(D))
+    if sqrt_D**2 == D:
+        print 0, sqrt_D
+        return
+    for i in range(int(math.sqrt(D)),0,-1):
+        if (D % i == 0):
+            n2 = D/i # second factor
+            b = int((n2 - i)/2.0) # possible b
+            a_2 = D + b*b # possible a^2
+            a = int(math.sqrt(a_2))
+            if (a*a == a_2 ) and (a**2 - b**2):
+                print b,a
+                return
+    print "impossible"
+
+if __name__ == '__main__':
+    main()
