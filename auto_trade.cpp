6e573976b3ac1e0832a6fdd4c010fcdd93b30a35
HermonMulat <hermonmulat@gmail.com>
2017-03-06 20:50:25 +0100

    https://open.kattis.com/problems/automatictrading

---
+++ auto_trade.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+int count(string s, int a, int b){
+    int c = 0;
+    int length = s.length();
+    while((b+c < length) && (s[a+c] == s[b+c])){
+        c++;
+    }
+    return c;
+}
+int main(int argc, char* argv[]){
+    //ifstream in;
+    //in.open(argv[1]);
+
+    int a,b;
+    string s;
+    cin >> s;
+    cin >> a; // dont need it - thus ignored
+    while(cin>>a>>b){
+        cout <<count(s,a,b)<<endl;
+    }
+    return 0;
+}
