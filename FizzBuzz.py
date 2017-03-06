11b9ecf3840fdbcb8dcb93ba82a8fcb35968f438
HermonMulat <hermonmulat@gmail.com>
2017-03-06 20:58:52 +0100

    https://open.kattis.com/problems/fizzbuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+import sys
+input_values = sys.stdin.readlines()[0].split()
+for i in range(len(input_values)):
+    input_values[i] = int (input_values[i])
+    
+for i in range(1,input_values[2]+1):
+
+    if (i % input_values[0] == 0):
+        if (i % input_values[1] == 0):
+            print "FizzBuzz"
+        else:
+            print "Fizz"
+    elif (i % input_values[1] == 0):
+        print "Buzz"
+    else:
+        print i
