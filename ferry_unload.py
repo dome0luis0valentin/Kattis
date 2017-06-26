fe76dd21db782c044bd6ab580e4e9444177fe1fa
HermonMulat <hermonmulat@gmail.com>
2017-06-26 02:24:21 -0400

    solves Ferry Unloading IV

--- ferry_unload.py
+++ ferry_unload.py
@@ -1,9 +1,37 @@
 import sys
+
+LEFT = "left"
+RIGHT = "right"
+
+def trips(left,right):
+    if len(left)==len(right):
+        if right[-1]==0:
+            return int(left[-1]!=0)
+        else:
+            return 2*len(left)
+    else:
+        return 2*(max(len(left),len(right))) - int(len(right)<len(left))
+
 def main():
     cases = int(sys.stdin.readline().strip())
     for i in range(cases):
         ferry_length, total_cars = map(int, sys.stdin.readline().split())
         ferry_length *= 100 # all units are in cm
-        trips = 0
-        total_length =
-        while ()
+        length_l = [0]
+        length_r = [0]
+        for car in range(total_cars):
+            size, side = sys.stdin.readline().split()
+            size = int(size)
+            if side == LEFT:
+                if (length_l[-1]+size < ferry_length):
+                    length_l[-1] += size
+                else:
+                    length_l.append(size)
+            else:
+                if (length_r[-1]+size < ferry_length):
+                    length_r[-1] += size
+                else:
+                    length_r.append(size)
+        print trips(length_l,length_r)
+
+main()
